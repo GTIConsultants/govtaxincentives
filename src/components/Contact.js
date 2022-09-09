76c2f5647f79710be395750fb36ff48f3f4656ce
GTIConsultants <100306815+GTIConsultants@users.noreply.github.com>
2022-09-09 12:10:45 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -32,7 +32,7 @@ class Contact extends Component {
                         <h3>Interested to find out whether the work you undertake qualifies for a government incentive?</h3>
                         <p>Request a consultation via the contact form. We can assess your eligibility, share expert insight, and 
                            potentially begin preparing your platform for future growth.</p>
-                        <p>Alternatively, call 07906 174 869 to speak to one of our expert advisors.</p>
+                        <p>Alternatively, call 020 8058 0990 to speak to one of our expert advisors.</p>
                     </div>
                     <div className="form-container">
                         <form onSubmit={this.sendEmail}>
